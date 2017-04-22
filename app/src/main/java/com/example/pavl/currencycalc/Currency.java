37374856c7705c756482cf73c3292ab2eed0f351
Pavel Sokolov <pavel.zepp@gmail.com>
2017-04-22 16:02:11 +0300

    model: minor update
    - superfluous fields are removed
    - getRubles is fixed to provide value per unit

--- app/src/main/java/com/example/pavl/currencycalc/Currency.java
+++ app/src/main/java/com/example/pavl/currencycalc/Currency.java
@@ -1,52 +1,31 @@
 package com.example.pavl.currencycalc;
 
-import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
 
-@Root(name = "Valute")
+@Root(name = "Valute", strict = false)
 public class Currency {
+    @Element(name = "CharCode")
+    private String charCode = "RUB";
 
-    @Attribute(name="ID")
-    private String _ID = "";
+    @Element(name = "Nominal")
+    private int amount = 1;
 
-    @Element(name="NumCode")
-    private int _NumCode = 0;
+    @Element(name = "Name")
+    private String name = "Rubble";
 
-    @Element(name="CharCode")
-    private String _CharCode = "RUB";
+    @Element(name = "Value")
+    private double rubbles = 1.0;
 
-    @Element(name="Nominal")
-    private int _Nominal = 1;
-
-    @Element(name="Name")
-    private String _Name = "Rubble";
-
-    @Element(name="Value")
-    private double _Rubbles = 1.0;
-
-    public int getNumCode ()
-    {
-        return _NumCode;
+    public String getCharCode() {
+        return charCode;
     }
 
-    public String getCharCode ()
-    {
-        return _CharCode;
+    public String getName() {
+        return name;
     }
 
-    public int getNominal ()
-    {
-        return _Nominal;
-    }
-
-    public String getName ()
-    {
-        return _Name;
-    }
-
-    public double getRubbles ()
-    {
-        return _Rubbles;
+    public double getRubbles() {
+        return rubbles / amount;
     }
 }
--- app/src/main/java/com/example/pavl/currencycalc/CurrencyList.java
+++ app/src/main/java/com/example/pavl/currencycalc/CurrencyList.java
@@ -4,28 +4,29 @@ import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Root;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 
-@Root(name = "ValCurs")
+@Root(name = "ValCurs", strict = false)
 public class CurrencyList {
-    @Attribute(name="Date")
-    private String _Date = "";
+    @Attribute(name = "Date")
+    private String date = "";
 
-    @Attribute(name="name")
-    private String _Name = "";
+    @ElementList(inline = true)
+    private List<Currency> currencies = new ArrayList<>();
 
-    @ElementList(inline=true)
-    private List<Currency> curList = new ArrayList<>();
-
-    public static double convert(Currency from, Currency to, double amount)
-    {
-        return amount * (from.getRubbles() / to.getRubbles());
+    public static double convert(Currency from, Currency to, double amount) {
+        if (from == to)
+            return amount;
+        else
+            return amount * (from.getRubbles() / to.getRubbles());
     }
 
-    public List<Currency> getCurrencies()
-    {
-        return curList;
+    public String getDate() {
+        return date;
+    }
+
+    public List<Currency> getCurrencies() {
+        return currencies;
     }
 }
