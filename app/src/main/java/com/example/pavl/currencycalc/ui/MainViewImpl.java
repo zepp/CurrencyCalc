3734af161ef8021685cbd5e6129c3829c144d12f
Pavel Sokolov <pavel.zepp@gmail.com>
2017-05-25 20:52:32 +0300

    ui: reworked
    - new layout
    - country flags
    - swap button

--- app/src/main/java/com/example/pavl/currencycalc/ui/MainViewImpl.java
+++ app/src/main/java/com/example/pavl/currencycalc/ui/MainViewImpl.java
@@ -4,6 +4,8 @@ import com.example.pavl.currencycalc.model.Currency;
 import com.example.pavl.currencycalc.model.CurrencyList;
 import com.example.pavl.currencycalc.R;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -12,6 +14,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -20,6 +24,8 @@ import java.util.List;
 
 public class MainViewImpl implements MainView {
     private final List<Currency> currencies = new ArrayList<>();
+    private final Resources resources;
+    private final String packageName;
     private Listener listener;
     private View root;
     private TextView originalTitle;
@@ -31,25 +37,42 @@ public class MainViewImpl implements MainView {
     private Spinner resultCurrency;
     private CurrencyAdapter currencyAdapter;
     private TextView dataDate;
+    private ImageView originalFlag;
+    private ImageView resultFlag;
+    private ImageButton swap;
 
     public MainViewImpl(LayoutInflater inflater, ViewGroup container) {
         root = inflater.inflate(R.layout.fragment_main, container, false);
+        Context context = root.getContext();
 
+        originalFlag = (ImageView) root.findViewById(R.id.original_flag);
         originalTitle = (TextView) root.findViewById(R.id.original_title);
         originalCurrency = (Spinner) root.findViewById(R.id.original_currency);
         originalAmount = (EditText) root.findViewById(R.id.original_amount);
+        swap = (ImageButton) root.findViewById(R.id.swap);
+        resultFlag = (ImageView) root.findViewById(R.id.result_flag);
         resultTitle = (TextView) root.findViewById(R.id.result_title);
         resultCurrency = (Spinner) root.findViewById(R.id.result_currency);
         resultAmount = (EditText) root.findViewById(R.id.result_amount);
         dataDate = (TextView) root.findViewById(R.id.date);
 
-        currencyAdapter = new CurrencyAdapter(root.getContext(), currencies);
+        currencyAdapter = new CurrencyAdapter(context, currencies);
 
         originalCurrency.setAdapter(currencyAdapter);
         originalCurrency.setOnItemSelectedListener(new OriginalCurrencyListener());
         originalAmount.addTextChangedListener(fromTextWatcher);
         resultCurrency.setAdapter(currencyAdapter);
         resultCurrency.setOnItemSelectedListener(new ResultCurrencyListener());
+        swap.setOnClickListener(new SwapListener());
+
+        resources = context.getResources();
+        packageName = context.getPackageName();
+
+    }
+
+    private int getFlagResourceId (Currency currency) {
+        String name = "ic_" + currency.getCharCode().toLowerCase();
+        return resources.getIdentifier(name, "drawable", packageName);
     }
 
     @Override
@@ -79,7 +102,7 @@ public class MainViewImpl implements MainView {
 
     @Override
     public void setResult(double amount) {
-        resultAmount.setText(String.format("%.2f", amount));
+        resultAmount.setText(String.format("%.1f", amount));
     }
 
     private class AmountTextWatcher implements TextWatcher {
@@ -110,7 +133,13 @@ public class MainViewImpl implements MainView {
         @Override
         public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
             Currency result = (Currency) adapterView.getItemAtPosition(i);
-            resultTitle.setText(result.getName());
+            int id = getFlagResourceId(result);
+            if (id == 0) {
+                resultFlag.setVisibility(View.GONE);
+            } else {
+                resultFlag.setVisibility(View.VISIBLE);
+                resultFlag.setImageResource(id);
+            }
             if (listener != null) {
                 listener.resultCurrencyChanged(result);
             }
@@ -125,7 +154,13 @@ public class MainViewImpl implements MainView {
         @Override
         public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
             Currency original = (Currency) adapterView.getItemAtPosition(i);
-            originalTitle.setText(original.getName());
+            int id = getFlagResourceId(original);
+            if (id == 0) {
+                originalFlag.setVisibility(View.GONE);
+            } else {
+                originalFlag.setVisibility(View.VISIBLE);
+                originalFlag.setImageResource(getFlagResourceId(original));
+            }
             if (listener != null) {
                 listener.originalCurrencyChanged(original);
             }
@@ -133,6 +168,16 @@ public class MainViewImpl implements MainView {
 
         @Override
         public void onNothingSelected(AdapterView<?> adapterView) {
+        }
+    }
+
+    private class SwapListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            int original = resultCurrency.getSelectedItemPosition();
+            int result = originalCurrency.getSelectedItemPosition();
+            resultCurrency.setSelection(result);
+            originalCurrency.setSelection(original);
         }
     }
 }
