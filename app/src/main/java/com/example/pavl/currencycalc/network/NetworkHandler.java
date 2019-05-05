379618f8e0683274dca01b9a043991d6076e8ba0
Pavel Sokolov <pavel.zeppa@yandex.ru>
2019-05-05 12:31:05 +0300

    NetworkHandler minor update
    - timeouts
    - context reference is removed

--- app/src/main/java/com/example/pavl/currencycalc/network/NetworkHandler.java
+++ app/src/main/java/com/example/pavl/currencycalc/network/NetworkHandler.java
@@ -8,6 +8,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -15,17 +16,20 @@ import okhttp3.Response;
 
 public final class NetworkHandler {
     private final static String URL = "http://www.cbr.ru/scripts/XML_daily.asp";
-    private final Context context;
     private final OkHttpClient client;
+    private final File cacheDir;
 
     public NetworkHandler(Context context) {
-        this.context = context;
-        this.client = new OkHttpClient();
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        cacheDir = context.getCacheDir();
+        builder.connectTimeout(5, TimeUnit.SECONDS);
+        builder.readTimeout(5, TimeUnit.SECONDS);
+        client = builder.build();
     }
 
     public File fetch() throws IOException {
         Request request = new Request.Builder().url(URL).build();
-        File file = new File(context.getCacheDir(), UUID.randomUUID().toString() + ".xml");
+        File file = new File(cacheDir, UUID.randomUUID().toString() + ".xml");
 
         try (OutputStream output = new FileOutputStream(file)){
             Response response = client.newCall(request).execute();
